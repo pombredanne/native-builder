e7d766f073bd07c9416b5fdbe6299cc7a8d55990
pombredanne <pombredanne@nexb.com>
2013-09-24 12:39:49 +0200

    Experimenting with mingw

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PACKAGE_NAME=zlib \
+PACKAGE_VERSION=1.2.8 \
+PACKAGE_URL=http://zlib.net \
+build
+
+PACKAGE_NAME=file \
+PACKAGE_VERSION=5.14 \
+PACKAGE_URL=ftp://ftp.astron.com/pub/file \
+build
+
+ 
