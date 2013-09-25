d9f20fdf3f34c39f95911c813cddfe5137666f34
Philippe Ombredanne <pombredanne@gmail.com>
2013-09-25 10:02:09 +0200

    glibc specila

--- setup.sh
+++ setup.sh
@@ -37,3 +37,20 @@ function build2 {
   echo "Done Building $PACKAGE"
   cd $HERE
 }
+
+function build3 {
+  PACKAGE=$PACKAGE_NAME-$PACKAGE_VERSION
+  PACKAGE_DOWNLOAD=$PACKAGE_URL/$PACKAGE.tar.gz
+  echo "Building $PACKAGE"
+  cd $HERE/build
+  curl --progress-bar $PACKAGE_DOWNLOAD | tar -zox
+  mkdir -p $PACKAGE-build
+  cd $PACKAGE-build
+  ../$PACKAGE/configure
+  make
+  DESTDIR=$HERE/dist make install
+  echo "Done Building $PACKAGE"
+  cd $HERE
+}
+
+
